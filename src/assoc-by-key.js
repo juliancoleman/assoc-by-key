27637be5c321741a114d2faa63fcee8b351fc383
Julian Coleman <julcol03@gmail.com>
2017-10-31 13:47:52 -0700

    fix a massive, dumb issue

--- src/assoc-by-key.js
+++ src/assoc-by-key.js
@@ -1,1 +1,13 @@
-module.exports = require("./assoc-by-key");
+const {
+  assoc,
+  compose,
+  map,
+  toPairs,
+} = require("ramda");
+
+const assocByKey = key1 => compose(
+  map(([key2, obj]) => assoc(key1, key2, obj)),
+  toPairs // eslint-disable-line
+);
+
+module.exports = assocByKey;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,1 @@
-const {
-  assoc,
-  compose,
-  map,
-  toPairs,
-} = require("ramda");
-
-const assocByKey = key1 => compose(
-  map(([key2, obj]) => assoc(key1, key2, obj)),
-  toPairs // eslint-disable-line
-);
-
-module.exports = assocByKey;
+module.exports = require("./assoc-by-key");
